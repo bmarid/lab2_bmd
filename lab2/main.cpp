dcbc5cdc5f6b5a281769bc017432124cb68b1496
bmarid <mary.baj@yandex.ru>
2020-03-27 15:40:50 +0700

    Initial Commit

---
+++ lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab2
+//
+//  Created by Мария Бай on 27/03/2020.
+//  Copyright © 2020 Мария Бай. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
